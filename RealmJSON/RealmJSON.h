5d50e758d3ee0f707dd02c557dd9accf7105733a
Andreas Kompanez <andreas.kompanez@beekeeper.io>
2017-08-03 18:37:44 +0200

    Adds carthage support

---
+++ RealmJSON/RealmJSON.h
@@ -0,0 +1,19 @@
+//
+//  RealmJSON.h
+//  RealmJSON
+//
+//  Created by Andreas Kompanez on 03.08.17.
+//  Copyright © 2017 Andreas Kompanez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RealmJSON.
+FOUNDATION_EXPORT double RealmJSONVersionNumber;
+
+//! Project version string for RealmJSON.
+FOUNDATION_EXPORT const unsigned char RealmJSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealmJSON/PublicHeader.h>
+
+
