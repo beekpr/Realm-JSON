a613ef8942b603f69c85cc4bec25f14194bcccb4
Andreas Kompanez <andreas.kompanez@beekeeper.io>
2018-01-30 17:09:41 +0100

    Fixes the project file

--- RealmJSON/RLMObject+JSON.h
+++ RealmJSON/RLMObject+JSON.h
@@ -22,7 +22,7 @@
 - (id)primaryKeyValue;
 + (id)primaryKeyValueFromJSONDictionary:(NSDictionary *)dictionary;
 
-- (void)performInTransaction:(void (^)(void))transaction;
+- (void)performInTransaction:(__attribute__((noescape)) void(^)(void))transaction;
 - (void)removeFromRealm;
 
 @end
---
+++ RealmJSON/RealmJSON.h
@@ -0,0 +1,19 @@
+//
+//  RealmJSON.h
+//  RealmJSON
+//
+//  Created by Andreas Kompanez on 30.01.18.
+//  Copyright © 2018 Beekeeper AG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RealmJSON.
+FOUNDATION_EXPORT double RealmJSONVersionNumber;
+
+//! Project version string for RealmJSON.
+FOUNDATION_EXPORT const unsigned char RealmJSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealmJSON/PublicHeader.h>
+
+
