e7cc6414d9a107b4def7bb15ddab73fcf34dac9e
Andreas Kompanez <andreas.kompanez@beekeeper.io>
2019-08-14 13:34:37 +0200

    Adds a remove dependencies helper script

---
+++ remove-dependencies.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+
+rm -rf Carthage/Build/iOS/Realm.framework
+rm -rf Carthage/Build/iOS/Realm.framework.dSYM
+rm -rf Carthage/Build/iOS/RealmSwift.framework
+rm -rf Carthage/Build/iOS/RealmSwift.framework.dSYM
